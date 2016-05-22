56f51b68c63341749ad3ca088b00b4345e51c43c
Bernhard Riegler <b.riegler@gentics.com>
2016-05-22 15:27:32 +0200

    changed relative paths

--- weatherwebapp/service-worker.js
+++ weatherwebapp/service-worker.js
@@ -1,22 +1,22 @@
 var dataCacheName = 'weatherData-v1';
 var cacheName = 'weatherPWA-step-celebrate-1';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/scripts/app.js',
-  '/images/clear.png',
-  '/images/cloudy-scattered-showers.png',
-  '/images/cloudy.png',
-  '/images/fog.png',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/partly-cloudy.png',
-  '/images/rain.png',
-  '/images/scattered-showers.png',
-  '/images/sleet.png',
-  '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
+  '/weatherwebapp',
+  '/weatherwebapp/index.html',
+  '/weatherwebapp/scripts/app.js',
+  '/weatherwebapp/images/clear.png',
+  '/weatherwebapp/images/cloudy-scattered-showers.png',
+  '/weatherwebapp/images/cloudy.png',
+  '/weatherwebapp/images/fog.png',
+  '/weatherwebapp/images/ic_add_white_24px.svg',
+  '/weatherwebapp/images/ic_refresh_white_24px.svg',
+  '/weatherwebapp/images/partly-cloudy.png',
+  '/weatherwebapp/images/rain.png',
+  '/weatherwebapp/images/scattered-showers.png',
+  '/weatherwebapp/images/sleet.png',
+  '/weatherwebapp/images/snow.png',
+  '/weatherwebapp/images/thunderstorm.png',
+  '/weatherwebapp/images/wind.png'
 ];
 
 self.addEventListener('install', function(e) {
